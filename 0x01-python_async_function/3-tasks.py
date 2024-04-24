7f2b01aeebe8c232b8f6beb85c6ecbe561a686b0
Kaleb Teshale <kalebteshale72@gmail.com>
2024-04-24 05:36:27 +0300

    kokok:

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-""" 3. Tasks
-"""
-
+'''Task 3's module.
+'''
 import asyncio
+
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
-    """ Take the integer on max delay and return the task
-    """
+    '''Creates an asynchronous task for wait_random.
+    '''
     return asyncio.create_task(wait_random(max_delay))
